3189343d93851105f78dba4e3694292cbf2a4b21
Michael J Cerce <137245672+MichaelJCerce@users.noreply.github.com>
2023-07-12 18:35:58 -0400

    Finish ExpenseDate.jsx layout

--- src/components/ExpenseDate.jsx
+++ src/components/ExpenseDate.jsx
@@ -1,5 +1,14 @@
-function ExpenseDate() {
-  return <div></div>;
+function ExpenseDate({ date }) {
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
+  return (
+    <div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+    </div>
+  );
 }
 
 export default ExpenseDate;
