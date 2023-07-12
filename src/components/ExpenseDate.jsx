7329b0d3b463779efde7d3159f092e6f1f86e90c
Michael J Cerce <137245672+MichaelJCerce@users.noreply.github.com>
2023-07-12 19:02:21 -0400

    Add ExpenseDate to ExpenseItem

--- src/components/ExpenseDate.jsx
+++ src/components/ExpenseDate.jsx
@@ -2,6 +2,7 @@ function ExpenseDate({ date }) {
   const month = date.toLocaleString("en-US", { month: "long" });
   const day = date.toLocaleString("en-US", { day: "2-digit" });
   const year = date.getFullYear();
+
   return (
     <div>
       <div>{month}</div>
