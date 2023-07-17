e12b62b46ea672c01c8217a3fb66b94db25cbb6d
Michael J Cerce <137245672+MichaelJCerce@users.noreply.github.com>
2023-07-17 15:22:50 -0400

    Replace vauge variable names

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -2,7 +2,7 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
-  const total = props.dataPoints
+  const totalExpenses = props.dataPoints
     .map((datapoint) => datapoint.value)
     .reduce((sum, term) => sum + term);
 
@@ -12,7 +12,7 @@ const Chart = (props) => {
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={total}
+          totalExpenses={totalExpenses}
           label={dataPoint.label}
         />
       ))}
